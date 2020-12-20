3b809321ae8547258f5c7fecad46684dfe5c177f
xFluke <mikoman1124@gmail.com>
2020-12-19 21:22:33 -0500

    Added comments on shrinkingplatform controller

--- Assets/_Scripts/ShrinkingPlatformController.cs
+++ Assets/_Scripts/ShrinkingPlatformController.cs
@@ -2,24 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Miko Man 101127881
+// Controller script for the Shrinking platform that shrinks or grows in size when the player steps on them
+
 public class ShrinkingPlatformController : MonoBehaviour
 {
-    bool movingUp;
+    // Floating variables
     Vector3 startingPos;
-    Vector3 startingLocalScale;
-
+    bool movingUp;
     public float moveSpeed;
     public float maxMoveDistance;
 
-    public bool isActive = false;
+    // Shrinking variables
+    Vector3 startingLocalScale;
+    public bool isActive;
     public float shrinkSpeed;
-
     [SerializeField]
     AudioSource shrinkingSound;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize variables
+        isActive = false;
         movingUp = true;
         startingPos = transform.position;
         startingLocalScale = transform.localScale;
@@ -30,34 +35,37 @@ public class ShrinkingPlatformController : MonoBehaviour
         Float();
 
         if (isActive) {
-            // Shrinking
-            if (!shrinkingSound.isPlaying) {
-                shrinkingSound.Play();
-            }
-
-
-            if (transform.localScale.x > 0) {
-                transform.localScale -= new Vector3(shrinkSpeed, 0, 0);
-            }
-            else {
-                transform.localScale = new Vector3(0, startingLocalScale.y, startingLocalScale.z);
-            }
+            Shrink();
         }
         else {
-            // Growing
+            Grow();
+        }
+    }
 
+    private void Grow() {
+        // Grow to original size
+        if (transform.localScale.x < startingLocalScale.x)
+            transform.localScale += new Vector3(shrinkSpeed, 0, 0);
+        else
+            transform.localScale = startingLocalScale;
+    }
 
-            if (transform.localScale.x < startingLocalScale.x)
-                transform.localScale += new Vector3(shrinkSpeed, 0, 0);
-            else
-                transform.localScale = startingLocalScale;
-
+    private void Shrink() {
+        // Shrink until localScale.x reaches 0
+        if (!shrinkingSound.isPlaying) {
+            shrinkingSound.Play();
         }
 
-
+        if (transform.localScale.x > 0) {
+            transform.localScale -= new Vector3(shrinkSpeed, 0, 0);
+        }
+        else {
+            transform.localScale = new Vector3(0, startingLocalScale.y, startingLocalScale.z);
+        }
     }
 
     private void Float() {
+        // Move up and down
         if (transform.position.y >= startingPos.y + maxMoveDistance || transform.position.y <= startingPos.y - maxMoveDistance) {
             movingUp = !movingUp;
         }
